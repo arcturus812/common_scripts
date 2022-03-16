15ba7c172448354726337ec391fd70d50f59a972
hwpark <arcturus812@gmail.com>
2022-03-16 20:37:13 +0900

    simple update

--- diff_kernel.sh
+++ diff_kernel.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 . ./common.sh --source-only
 ENV_TARGET=env_kernel.rc
+DATE=`date +%Y%m%d%H%M`
+OUTPUT="./kernel_patches/kernel_$DATE.patch"
 source $ENV_TARGET &&
     echo -e "[PHW]ENV sourced ${myGR}$ENV_TARGET${myNC}"
 
 find $KERNEL_ORIGIN $KERNEL_HOME -type f -not -name '*.[ch]' > excludes &&
     echo -e "[PHW]kernel exclude list extracted"
 
-diff -wbur $KERNEL_ORIGIN $KERNEL_HOME -X excludes |grep -v Only |grep -v Binary > coretime_kernel.patch &&
+diff -wbur $KERNEL_ORIGIN $KERNEL_HOME -X excludes |grep -v Only |grep -v Binary > $OUTPUT &&
     echo -e "[PHW]kernel diff successed"
+
+PATCH_SIZE=`stat --printf="%s" $OUTPUT`
+if [ $PATCH_SIZE -eq 0 ]
+then
+    echo -e "[PHW]nothing changed, remove patch file"
+    rm -rf $OUTPUT
+fi
+
 
--- kernel_reinstall.sh
+++ kernel_reinstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 . ./common.sh --source-only
 ENV_TARGET=env_kernel.rc
-myAnswer=n
 source $ENV_TARGET &&
     echo -e "[PHW]ENV sourced ${myGR}$ENV_TARGET${myNC}"
 CUR_DIR=`pwd`
@@ -9,15 +8,16 @@ echo -e "build target: $KERNEL_HOME"
 echo -e "[PHW]Enable automotive ${myRED}REBOOT?${myNC} Yes/No"
 askY
 
+./diff_kernel.sh
 cd $KERNEL_HOME &&
     echo -e "[PHW]move dir into ${myBL}$KERNEL_HOME${myNC}"
 
 # cd $KERNEL_ORIGIN &&
 #     echo -e "[PHW]move dir into ${myBL}$KERNEL_ORIGIN${myNC}"
 
-make -j$(nproc) | tee build.log &&
-    make modules_install -j$(nproc) | tee module_install.log &&
-    make install -j$(nproc) | tee install.log&&
+make -j16 | tee build.log &&
+    make modules_install -j16 | tee module_install.log &&
+    make install -j16 | tee install.log&&
     echo -e "[PHW]${myGR}SUCCESS${myNC} to kernel install" &&
     update-grub2 > /dev/null &&
     echo -e "[PHW]${myGR}SUCCESS${myNC} to grub2-update"
